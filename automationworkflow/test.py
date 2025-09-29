8a0888d09c64f84f4dc32c055eceae76bb067faa
vrishab2112 <vishnu21may@gmail.com>
2025-09-29 15:06:16 +0530

    chore: remove test.py

--- automationworkflow/test.py
+++
@@ -1,5 +0,0 @@
-import os, requests
-key ="AIzaSyB9Idtjje00e0C7t3jmtYzhOxFs9X2hdFY"
-video = "dQw4w9WgXcQ"
-r = requests.get("https://www.googleapis.com/youtube/v3/commentThreads", params={"part":"snippet","maxResults":1,"videoId":video,"key":key}, timeout=30)
-print(r.status_code, r.json())
